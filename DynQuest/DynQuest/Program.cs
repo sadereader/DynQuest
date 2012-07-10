92aa079800e259fe47252a52ff158dcc4960050e
Ced@lynx <Ced@fennecweb.net>
2012-07-10 07:14:38 +0200

    Small changes
    To show how github pull requests work mainly c:

--- DynQuest/DynQuest/Program.cs
+++ DynQuest/DynQuest/Program.cs
@@ -7,9 +7,18 @@ namespace DynQuest
 {
     class Program
     {
+        public static bool Run = true;
         static void Main(string[] args)
         {
-            Console.Write("I once fucked a cheesegrater with my foot.");
+            Console.WriteLine("Initializing DynQuest version {0}.","version.goes.here.");
+            while (Run)
+            {
+                //magic happens here
+
+            }
+
+
+            Console.Write("DynQuest has reached exit state, press any key to close DynQuest");
             Console.ReadKey(true);
         }
     }
