64318d5b3f48a1c757b921223eaff6de79790c4e
Cobalt Husky <sadereader@gmail.com>
2012-07-09 21:48:30 -0700

    BEAN CURD.

---
+++ DynQuest/DynQuest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DynQuest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("I once fucked a cheesegrater with my foot.");
+            Console.ReadKey(true);
+        }
+    }
+}
