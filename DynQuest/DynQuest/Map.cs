e63655a9c283da968c55ba708aeb8cc839d446d3
Ced@lynx <Ced@fennecweb.net>
2012-08-16 13:23:24 +0200

    Commit before reinstall
    Gonna reinstall PC, gotta commit my stuff, nothing really exciting here

---
+++ DynQuest/DynQuest/Map.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+namespace DynQuest
+{
+    class Map
+    {
+        public Tile[,] Board;
+        public Map()
+        {
+            //Initialize the board
+            Board = new Tile[100, 100];
+
+            
+        }
+
+        public class Tile 
+        {
+            public Bitmap bitmap;
+            public Dictionary<string,string> properties;
+
+            public Tile()
+            {
+                bitmap = new Bitmap(50, 50, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                properties = new Dictionary<string, string>();
+            }
+            
+
+        
+        }
+    }
+}
